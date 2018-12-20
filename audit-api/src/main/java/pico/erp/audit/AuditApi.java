f64cc6b2520b29cfebe6bd76e7d2278abb30fb8f
Ethan <kkojaeh@gmail.com>
2018-12-20 13:45:50 +0900

    모듈 종속 관계 제어 방식 변경

---
+++ audit-api/src/main/java/pico/erp/audit/AuditApi.java
@@ -0,0 +1,9 @@
+package pico.erp.audit;
+
+import pico.erp.shared.ApplicationId;
+
+public final class AuditApi {
+
+  public static ApplicationId ID = ApplicationId.from("audit");
+
+}
--- audit-impl/src/main/java/pico/erp/audit/AuditApplication.java
+++ audit-impl/src/main/java/pico/erp/audit/AuditApplication.java
@@ -1,16 +1,17 @@
 package pico.erp.audit;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Collections;
 import java.util.Properties;
+import java.util.Set;
 import lombok.extern.slf4j.Slf4j;
 import org.javers.core.Javers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.scheduling.TaskScheduler;
+import pico.erp.shared.ApplicationId;
 import pico.erp.shared.ApplicationInitializer;
 import pico.erp.shared.ApplicationStarter;
 import pico.erp.shared.Public;
@@ -24,16 +25,11 @@ public class AuditApplication implements ApplicationStarter {
 
   public static final String CONFIG_NAME = "audit/application";
 
-  public static final String CONFIG_NAME_PROPERTY = "spring.config.name=audit/application";
-
   public static final Properties DEFAULT_PROPERTIES = new Properties();
 
   static {
     DEFAULT_PROPERTIES.put("spring.config.name", CONFIG_NAME);
   }
-
-  @Autowired
-  private ApplicationContext applicationContext;
 
   public static SpringApplication application() {
     return new SpringApplicationBuilder(AuditApplication.class)
@@ -49,8 +45,15 @@ public class AuditApplication implements ApplicationStarter {
   @Autowired
   private AuditConfigurer auditConfigurer;
 
-  @Autowired
-  private TaskScheduler taskScheduler;
+  @Override
+  public Set<ApplicationId> getDependencies() {
+    return Collections.emptySet();
+  }
+
+  @Override
+  public ApplicationId getId() {
+    return AuditApi.ID;
+  }
 
   @Override
   public boolean isWeb() {
@@ -65,11 +68,6 @@ public class AuditApplication implements ApplicationStarter {
   @Bean
   public ObjectMapper objectMapper() {
     return new ObjectMapper();
-  }
-
-  @Override
-  public int getOrder() {
-    return Integer.MIN_VALUE;
   }
 
   @Bean
